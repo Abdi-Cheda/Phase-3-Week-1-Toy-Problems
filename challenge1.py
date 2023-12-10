694b88107fc85f44d0c5ab54a0b4eccaee078eb5
Abdi-Cheda <abdicheda75@gmail.com>
2023-12-10 19:01:37 +0300

    commit

---
+++ challenge1.py
@@ -0,0 +1,22 @@
+def convert_to_24_hour_format(hour, minute, period):
+    if period.lower() == "am":
+        if hour == 12:
+            hour = 0
+    else:
+        if hour != 12:
+            hour += 12
+
+    return "{:02d}{:02d}".format(hour, minute)
+
+def get_user_input():
+    hour = int(input("Enter the hour (1-12): "))
+    minute = int(input("Enter the minute (0-59): "))
+    period = input("Enter 'am' or 'pm': ").lower()
+
+    return hour, minute, period
+
+user_hour, user_minute, user_period = get_user_input()
+
+result = convert_to_24_hour_format(user_hour, user_minute, user_period)
+
+print("The time in 24-hour format is:", result)
